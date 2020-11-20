a333db54ac43a8db47a66df09666d572ffad44f0
Matheus Diogenes <matheusdiogenes98@gmail.com>
2020-11-20 19:35:44 -0300

    colocando em funções

--- src/cliente.py
+++ src/cliente.py
@@ -3,13 +3,16 @@
 import socket
 import uuid
 
-HOST = 'localhost'
-PORT = 8081
+def main():
+  HOST = 'localhost'
+  PORT = 8081
+  tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  orig = (HOST, PORT)
+  tcp.connect(orig)
+  tcp.send(str.encode(uuid.uuid4().hex))
+  data = tcp.recv(1024)
+  print(f'Dados: {data.decode()}')
 
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-tcp.connect(orig)
-tcp.send(str.encode(uuid.uuid4().hex))
-data = tcp.recv(1024)
 
-print(f'Dados: {data.decode()}')
+if __name__ == "__main__":
+  main()
--- src/servidor.py
+++ src/servidor.py
@@ -1,37 +1,27 @@
 #!/usr/bin/env python3
 
 import socket
+def main():
+  HOST = 'localhost'
+  PORT = 8081
 
-HOST = 'localhost'
-PORT = 8081
+  tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  orig = (HOST, PORT)
+  tcp.bind(orig)
+  tcp.listen()
 
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-tcp.bind(orig)
-tcp.listen()
-# print('Aguardando conexão...')
-# conn, cliente = tcp.accept()
-# print(f'Conectado com o cliente: {cliente}')
-
-
-# while True:
-#   data = conn.recv(1024)
-#   if not data:
-#     print('Fechando conexão')
-#     conn.close()
-#     break
-#   conn.sendall(data)
-
-
-while True:
-  print('Aguardando conexão...')
-  conn, cliente = tcp.accept()
-  print(f'Conectado com o cliente: {cliente}')
   while True:
-    data = conn.recv(1024)
-    if not data:
-      print('Fechando conexão')
-      conn.close()
-      break
-    print(f'Cliente: {cliente}\nDados:{data}')
-    conn.sendall(data)  
+    print('Aguardando conexão...')
+    conn, cliente = tcp.accept()
+    print(f'Conectado com o cliente: {cliente}')
+    while True:
+      data = conn.recv(1024)
+      if not data:
+        print('Fechando conexão')
+        conn.close()
+        break
+      print(f'Cliente: {cliente}\nDados:{data}')
+      conn.sendall(data)  
+
+if __name__ == "__main__":
+  main()
