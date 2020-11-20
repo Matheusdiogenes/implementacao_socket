22ae6b19bcfeb5d31bd759eb3d3c399a56e74e33
Matheus Diogenes <matheusdiogenes98@gmail.com>
2020-11-20 13:50:39 -0300

    gerando token

--- src/cliente.py
+++ src/cliente.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+import uuid
 
 HOST = 'localhost'
 PORT = 8081
@@ -8,7 +9,7 @@ PORT = 8081
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.connect(orig)
-tcp.send(str.encode('Sou cliente'))
+tcp.send(str.encode(uuid.uuid4().hex))
 data = tcp.recv(1024)
 
 print(f'Dados: {data.decode()}')
