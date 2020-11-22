6a6ef9e73fcb03fa4dd7c2b6804057085ff542b9
Matheus Diogenes <matheusdiogenes98@gmail.com>
2020-11-22 14:04:06 -0300

    informando ao cliente a quantidade de tokens

--- src/cliente.py
+++ src/cliente.py
@@ -9,9 +9,15 @@ def main():
   tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   orig = (HOST, PORT)
   tcp.connect(orig)
-  tcp.send(str.encode(uuid.uuid4().hex))
-  data = tcp.recv(1024)
-  print(f'Dados: {data.decode()}')
+  # tcp.send(str.encode(uuid.uuid4().hex))
+  while True:
+    print(f'1) Solicitar Token (3 -1) \n2) Devolver token (5 X) \n3) Solicita quantidade de recursos disponíveis (1 -1)   \n')
+    msg = input('Insira a opção escolhida:  ')
+    tcp.send(str.encode(msg))
+    # data = tcp.recv(1024)    
+    info = tcp.recv(1024)
+    print(info.decode('utf-8'))
+
 
 
 if __name__ == "__main__":
--- src/servidor.py
+++ src/servidor.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
 
 import socket
+import threading
+
 def main():
   HOST = 'localhost'
   PORT = 8081
+  orig = (HOST, PORT)
 
   tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  orig = (HOST, PORT)
   tcp.bind(orig)
-  tcp.listen()
-
+  tcp.listen()  
+  qtde_token = int(input("Quantidade de token: "))
   while True:
     print('Aguardando conexão...')
     conn, cliente = tcp.accept()
     print(f'Conectado com o cliente: {cliente}')
-    while True:
-      data = conn.recv(1024)
-      if not data:
-        print('Fechando conexão')
-        conn.close()
-        break
-      print(f'Cliente: {cliente}\nDados:{data}')
-      conn.sendall(data)  
+    data = conn.recv(1024)    
+    print(f'Cliente: {cliente}\n')    
+    if '3' == data.decode('utf-8'):      
+      info = (str(qtde_token) + ' tokens disponiveis')
+      conn.send(info.encode('utf-8')) # Envia mensagem
+    qtde_token -= 1    
+    conn.sendall(data)  
+    
 
 if __name__ == "__main__":
   main()
